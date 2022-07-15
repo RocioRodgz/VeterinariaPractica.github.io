3ee2e84ba79037e88dd5abd0d11f21c6eae0d3ae
Rocio Rodriguez <102255720+RocioRodgz@users.noreply.github.com>
2022-07-14 23:51:47 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const menu=document.querySelector(".nav-menu")
+const toggle=document.querySelector(".toggle")
+
+
+toggle.addEventListener("click",()=>{
+menu.classList.toggle("nav-menu-visible")
+})
